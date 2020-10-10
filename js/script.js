e46bd28f133664ea3a4f8381cdc84c83b65f5aa9
Rovine1999 <rovinewanjala99@gmail.com>
2020-10-10 16:28:23 +0300

    modified

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
-
 function myFunction() {
-    const sideA= parseInt(document.getElementById("side1").value());
-    const sideB= parseInt(document.getElementById("side2").value());
-    const sideC= parseInt(document.getElementById("side3").value());
-   if (((sideA + sideB) > sideC) && ((sideB + sideC) > sideA) && ((sideC + sideA) > sideB)) {
-   alert("This is a Triangle")
- } else if((sideA === sideB) && (sideA != sideC) || (sideA === sideC) && (sideA != sideB)) {
-   alert("This is an isosceles Triangle")
- } else if((sideB === sideA) && (sideB != sideC) || (sideB === sideC) && (sideB != sideA)) {
-    alert("This is an isosceles Triangle")
-}else if((sideC === sideA) && (sideC != sideB) || (sideC === sideB) && (sideC != sideA)) {
-    alert("This is an isosceles Triangle")
- }else if((sideA != sideB) && (sideB != sideC) && (sideC != sideA)) {
-    alert("This is an scalene Triangle")
- }else if((sideA === sideB) && (sideA === sideC) (sideB === sideC)) {
-     alert("This is an equilateral triangle")
- }
+var sideA= parseInt(document.getElementById("side1").value);
+var sideB= parseInt(document.getElementById("side2").value);
+var sideC= parseInt(document.getElementById("side3").value);
+
+if((sideA === sideB) && (sideB === sideC) && (sideC === sideA)) {
+    alert("this an equilateral triangle");
 }
+else if((sideA === sideB) || (sideB === sideC) && (sideC != sideA)) {
+    alert("this is an isosceles triangle");
+   }
+   else if((sideA != sideB) && (sideB != sideC) && (sideC != sideA)) {
+       alert("this is a scalene triangle")
+   };
+}
+
